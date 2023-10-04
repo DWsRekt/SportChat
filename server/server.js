0dfbc699a0078ba83f673dd02e0315d14c33d18d
DWsRekt <thedavidlord@yahoo.de>
2023-10-04 23:27:35 +0200

    🙈: added .env to gitignore

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-const express = require('express')
+import {config} from 'dotenv'
+
+config({path : '../.env'})
+
+import express from 'express'
+
 const app = express()
+const PORT = process.env.BACKEND_PORT || 5001
+// const mongoose = require('mongoose')
+
+console.log(process.env.BACKEND_PORT)
+
+
+// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
 
 app.get("/api", (req, res) =>{
     res.json({"users": ["u1", "u2", "u2"]})
 })
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
